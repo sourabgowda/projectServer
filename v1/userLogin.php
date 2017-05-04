a77f11350675fc3f59f42192e09c324da7b8b8dc
Nikith Shetty <shettynikith3@gmail.com>
2017-05-04 20:04:01 +0530

    index added

--- v1/userLogin.php
+++ v1/userLogin.php
@@ -4,12 +4,12 @@ require_once '../includes/DbOperations.php';
 
 $response = array(); 
 
-if($_SERVER['REQUEST_METHOD']=='POST'){
-	if(isset($_POST['name']) and isset($_POST['password'])){
+if($_SERVER['REQUEST_METHOD']=='GET'){
+	if(isset($_GET['name']) and isset($_GET['password'])){
 		$db = new DbOperations(); 
 
-		if($db->userLogin($_POST['name'], $_POST['password'])){
-			$user = $db->getUserByUsername($_POST['name']);
+		if($db->userLogin($_GET['name'], $_GET['password'])){
+			$user = $db->getUserByUsername($_GET['name']);
 			$response['error'] = false; 
 			$response['name'] = $user['name'];
 			$response['email'] = $user['email'];
